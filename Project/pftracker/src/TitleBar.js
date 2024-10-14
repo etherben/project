28cc64c7976117c6316d7e4f927fab134b27cffd
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-10-14 11:45:23 +0100

    Added title Bar to front page

---
+++ Project/pftracker/src/TitleBar.js
@@ -0,0 +1,10 @@
+import './TitleBar.css';
+
+function TitleBar(){
+    return(<div className="title-bar" >
+        <h1>Personal Finance Tracker</h1>
+    </div>
+    );
+}
+
+export default TitleBar
