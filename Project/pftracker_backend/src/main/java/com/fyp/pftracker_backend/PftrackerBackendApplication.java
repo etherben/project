c5c3e6bc6e548bfde99985122d3ad4c29689801a
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-10-22 11:58:35 +0100

    Created springboot backend and sorted file structure

---
+++ Project/pftracker_backend/src/main/java/com/fyp/pftracker_backend/PftrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.pftracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PftrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PftrackerBackendApplication.class, args);
+	}
+
+}
---
+++ Project/pftracker_backend/src/test/java/com/fyp/pftracker_backend/PftrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.pftracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
