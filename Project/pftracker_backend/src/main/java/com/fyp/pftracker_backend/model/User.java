77766ef0aa376a2bc76ae09b7e9bf4ddc70c37a6
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-11-13 17:54:12 +0000

    put database inside docker container, backend can connect to and save users.

--- Project/pftracker_backend/src/main/java/com/fyp/pftracker_backend/model/User.java
+++ Project/pftracker_backend/src/main/java/com/fyp/pftracker_backend/model/User.java
@@ -1,41 +1,50 @@
 package com.fyp.pftracker_backend.model;
-import jakarta.persistence.*;
 
-@Entity
-@Table (name = "login")
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "login")  // MongoDB collection name
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    @Column
+    private String id;  // All string as MongoDB uses String for Id
+
     private String email;
-    @Column
     private String username;
-    @Column
     private String password;
 
-    public User(){}
-    public long getId() {
+
+    public User() {}
+
+    // Getters and setters
+    public String getId() {
         return id;
     }
-    public void setId(long id) {
+
+    public void setId(String id) {
         this.id = id;
     }
+
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
--- Project/pftracker_backend/src/main/java/com/fyp/pftracker_backend/repository/UserRepo.java
+++ Project/pftracker_backend/src/main/java/com/fyp/pftracker_backend/repository/UserRepo.java
@@ -1,16 +1,11 @@
 package com.fyp.pftracker_backend.repository;
 
 import java.util.Optional;
-
 import com.fyp.pftracker_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-
+public interface UserRepo extends MongoRepository<User, String> {
     Optional<User> findByUsername(String username);
-
-
 }
