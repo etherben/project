87211ae2708211b9118cb24a340dfcf8899ed0b8
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-11-14 14:36:32 +0000

    User Login endpoint created

--- Project/pftrackerback/src/main/java/BE/pftrackerback/Service/UserService.java
+++ Project/pftrackerback/src/main/java/BE/pftrackerback/Service/UserService.java
@@ -7,7 +7,14 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+    private final User admin = new User("admin", "password", "adminemail");
+    //hardcoded admin user for testing
     private final List<User> users = new ArrayList<>();
+
+    public UserService() {
+        users.add(admin); //adds admin user to list from start
+    }
 
     public User createUser(String username, String password, String email) {
         User user = new User(username, password, email);
@@ -19,4 +26,12 @@ public class UserService {
         return users;
     }
 
+    public User loginUser(String username, String password) {
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
