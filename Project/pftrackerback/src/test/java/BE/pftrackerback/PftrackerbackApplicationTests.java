0401d7c484099512f7b136fa769e6343eda6464e
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-11-14 12:18:57 +0000

    Created gradle project for backend with getmapping

---
+++ Project/pftrackerback/src/test/java/BE/pftrackerback/PftrackerbackApplicationTests.java
@@ -0,0 +1,13 @@
+package BE.pftrackerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PftrackerbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
