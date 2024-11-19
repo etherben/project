486389da7d9c97085d5fd829153dce48fb8fe1a4
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-11-19 10:54:20 +0000

    Add single transaction and get all transactions functionality

--- Project/pftrackerback/src/main/java/BE/pftrackerback/Service/TransactionService.java
+++ Project/pftrackerback/src/main/java/BE/pftrackerback/Service/TransactionService.java
@@ -1,4 +1,19 @@
 package BE.pftrackerback.Service;
 
+import BE.pftrackerback.Model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TransactionService {
+
+    private List<Transaction> transactions = new ArrayList<>();
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
 }
--- Project/pftrackerback/src/test/java/BE/pftrackerback/ServiceTest/TransactionServiceTest.java
+++ Project/pftrackerback/src/test/java/BE/pftrackerback/ServiceTest/TransactionServiceTest.java
@@ -1,16 +1,41 @@
 package BE.pftrackerback.ServiceTest;
 
+import BE.pftrackerback.Model.Transaction;
 import BE.pftrackerback.Service.TransactionService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
 public class TransactionServiceTest {
-
+    @Autowired
     private TransactionService transactionService;
 
     @BeforeEach
-    public void setUp() {
-        transactionService = new TransactionService();
+    public void setUp() {}
+
+    @Test
+    public void testAddTransaction() {
+        // Prepare the transaction
+        Transaction transaction = new Transaction();
+        transaction.setId("user1");
+        transaction.setTransactionDate(new Date());
+        transaction.setAmount(100.0);
+
+        // Add the transaction
+        transactionService.addTransaction(transaction);
+
+        // Verify that the transaction was added
+        List<Transaction> transactions = transactionService.getTransactions();
+        assertNotNull(transactions);
+        assertEquals(1, transactions.size());
+        assertEquals("user1", transactions.get(0).getId()); // Ensure userId is correctly linked
     }
-
-
 }
