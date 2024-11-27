8a65f385b0fef459f59f6b989ac91ae3cb16aabb
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2024-11-27 18:04:34 +0000

    added cross origin

--- Project/pftrackerback/src/main/java/BE/pftrackerback/Controllers/TransactionController.java
+++ Project/pftrackerback/src/main/java/BE/pftrackerback/Controllers/TransactionController.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:3000")
 @RequestMapping("/transactions")
 public class TransactionController {
 
