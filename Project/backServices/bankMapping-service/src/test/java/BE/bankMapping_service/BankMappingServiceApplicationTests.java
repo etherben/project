5747284ec9ae299a496db498f1e02549b10ee744
Ben Etheridhe <benjamin.etheridge.2022@live.rhul.ac.uk>
2025-03-04 12:58:18 +0000

    Empty bankMapping-Service created

---
+++ Project/backServices/bankMapping-service/src/test/java/BE/bankMapping_service/BankMappingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package BE.bankMapping_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMappingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
